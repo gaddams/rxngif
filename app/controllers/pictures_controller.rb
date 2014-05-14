4fa3e912eec752fec9f9e0332eb0f0a5ab0cfd04
gaddams <sgaddam2015@kellogg.northwestern.edu>
2014-05-14 02:10:15 -0500

    multiple pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -2,4 +2,9 @@ class PicturesController < ApplicationController
   def show
   	@picture = Picture.find_by(:id => params[:id])
   end
+
+  def index
+  	@list_of_all_pictures = Picture.all
+  end
+
 end
